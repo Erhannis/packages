72d9111407f09bfa25b47d357e22f2f2be7408a2
Erhannis Kirran <eyeillus@gmail.com>
2022-10-10 23:01:41 -0400

    Wrapped mdns interface attachment with try/catch, so one bad interface doesn't spoil the bunch

--- packages/multicast_dns/lib/multicast_dns.dart
+++ packages/multicast_dns/lib/multicast_dns.dart
@@ -131,31 +131,35 @@ class MDnsClient {
 
     for (final NetworkInterface interface in interfaces) {
       // Create a socket for sending on each adapter.
-      final InternetAddress targetAddress = interface.addresses[0];
-      final RawDatagramSocket socket = await _rawDatagramSocketFactory(
-        targetAddress,
-        selectedMDnsPort,
-        reuseAddress: true,
-        reusePort: true,
-        ttl: 255,
-      );
-      _sockets.add(socket);
-      // Ensure that we're using this address/interface for multicast.
-      if (targetAddress.type == InternetAddressType.IPv4) {
-        socket.setRawOption(RawSocketOption(
-          RawSocketOption.levelIPv4,
-          RawSocketOption.IPv4MulticastInterface,
-          targetAddress.rawAddress,
-        ));
-      } else {
-        socket.setRawOption(RawSocketOption.fromInt(
-          RawSocketOption.levelIPv6,
-          RawSocketOption.IPv6MulticastInterface,
-          interface.index,
-        ));
+      try {
+        final InternetAddress targetAddress = interface.addresses[0];
+        final RawDatagramSocket socket = await _rawDatagramSocketFactory(
+          targetAddress,
+          selectedMDnsPort,
+          reuseAddress: true,
+          reusePort: true,
+          ttl: 255,
+        );
+        _sockets.add(socket);
+        // Ensure that we're using this address/interface for multicast.
+        if (targetAddress.type == InternetAddressType.IPv4) {
+          socket.setRawOption(RawSocketOption(
+            RawSocketOption.levelIPv4,
+            RawSocketOption.IPv4MulticastInterface,
+            targetAddress.rawAddress,
+          ));
+        } else {
+          socket.setRawOption(RawSocketOption.fromInt(
+            RawSocketOption.levelIPv6,
+            RawSocketOption.IPv6MulticastInterface,
+            interface.index,
+          ));
+        }
+        // Join multicast on this interface.
+        incoming.joinMulticast(_mDnsAddress!, interface);
+      } catch (e) {
+        print("error in mdns $e");
       }
-      // Join multicast on this interface.
-      incoming.joinMulticast(_mDnsAddress!, interface);
     }
     incoming.listen((RawSocketEvent event) => _handleIncoming(event, incoming));
     _started = true;
